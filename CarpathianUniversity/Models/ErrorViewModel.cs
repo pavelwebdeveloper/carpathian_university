347d80d8b643b9211d9dc5f5636ba88eaaf0705e
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2022-11-19 20:04:54 -0700

    Finished Carpathian University
    Finished Carpathian University project

---
+++ CarpathianUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarpathianUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
